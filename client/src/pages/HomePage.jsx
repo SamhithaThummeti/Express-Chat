023b3fd6d882f6d05e710a6ca777dcad159e7e86
SamhithaThummeti <samhithathummeti@gmail.com>
2025-10-19 17:07:38 +0530

    css changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   const { selectedUser } = useContext(ChatContext);
 
   return (
-    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] bg-[url('/bgImage.svg')] bg-contain">
       <div
         className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${
           selectedUser
